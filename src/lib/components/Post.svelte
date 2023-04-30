f15e956fcff436ad3f9f1cfc441776177c2a786d
Roi <68617740+Roizor@users.noreply.github.com>
2023-04-30 10:08:35 -0500

    fix up looks + add formatting

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -5,12 +5,11 @@
 </script>
 
 <form action="/request" method="POST">
-    <div class="bg-slate-200 p-5 w-min">
+    <div class="bg-slate-200 p-5 w-min rounded-xl">
         <img src={post.image_src} alt="Photo of {post.name}">
         <h1>{post.name}</h1>
         <input name="uid" value="UserID" hidden />
         <input name="id" value={post.id} hidden />
-        <input type="submit" value="Request" />
-        {post.id}
+        <input type="submit" value="Request" class="p-5 rounded-md bg-slate-100" />
     </div>
 </form>
