0b9f85724e57235ffd9568df13487a0f41fd4e1f
Roi <68617740+Roizor@users.noreply.github.com>
2023-04-29 22:26:56 -0500

    some things

---
+++ src/routes/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+    let pages = [
+        {
+            'name': 'Home',
+            'route': '/'
+        },
+        {
+            'name': 'Profile',
+            'route': '/me'
+        },
+        {
+            'name': 'Login',
+            'route': '/login'
+        }
+    ]
+</script>
+
+<div class="nav bg-slate-500 p-5 grid grid-flow-col">
+    {#each pages as { name, route }}
+    <a class="bg-slate-300 w-min p-2 rounded-md" href={route}>{name}</a>
+    {/each}
+</div>
