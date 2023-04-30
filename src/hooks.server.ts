bdb09e3acc57907b051eccd7abb50eccdb8cf3e5
Roi <68617740+Roizor@users.noreply.github.com>
2023-04-30 11:20:57 -0500

    it works

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,11 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import Discord from "@auth/core/providers/discord"
+import { DISCORD_ID, DISCORD_SECRET, GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [
+	GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
+	Discord({ clientId: DISCORD_ID, clientSecret: DISCORD_SECRET })
+],
 })
